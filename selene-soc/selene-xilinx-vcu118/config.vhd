4bcd82b8a0279401d2427e4be55078e53ed486fe
Daniel Grosu <daniel.grosu@continental-corporation.com>
2026-03-02 14:09:36 +0200

    AXI C2C initial integration

--- selene-soc/selene-xilinx-vcu118/config.vhd
+++ selene-soc/selene-xilinx-vcu118/config.vhd
@@ -325,9 +325,9 @@ package config is
 
 -- AXI xbar configuration
 --The following line "CFG_AXI_N_INITIATORS" must not contain labels, logic operations or comments due to preprocesor scripts
---the current value "8" corresponds to 1(gpp) + 6 (HLSinf have 6 ports) + CFG_AXI_SAFETI_EN
+--the current value "7" corresponds to 1(gpp) + 6 (HLSinf have 6 ports)
   constant CFG_AXI_N_INITIATORS : integer := 8;
-  constant CFG_AXI_N_TARGETS : integer := 1;
+  constant CFG_AXI_N_TARGETS : integer := 2;
 -- AXI LITE xbar configuration
   constant CFG_AXI_LITE_N_INITIATORS : integer := 1;
   constant CFG_AXI_LITE_N_TARGETS : integer := CFG_AXI_N_ACCELERATORS;
--- selene-soc/selene-xilinx-vcu118/selene_soc.vhd
+++ selene-soc/selene-xilinx-vcu118/selene_soc.vhd
@@ -90,6 +90,13 @@ entity selene_soc is
     emdc        : out   std_ulogic;
     eint        : in    std_ulogic;
     erst        : out   std_ulogic;
+    -- AXI Chip2Chip physical link (FMC)
+    axi_c2c_selio_rx_data_in_0       : in  std_logic_vector(14 downto 0);
+    axi_c2c_selio_rx_diff_clk_in_p_0 : in  std_logic;
+    axi_c2c_selio_rx_diff_clk_in_n_0 : in  std_logic;
+    axi_c2c_selio_tx_data_out_0      : out std_logic_vector(14 downto 0);
+    axi_c2c_selio_tx_diff_clk_out_p_0: out std_logic;
+    axi_c2c_selio_tx_diff_clk_out_n_0: out std_logic;
     -- UART
     dsurx        : in    std_ulogic;
     dsutx        : out   std_ulogic;
@@ -199,6 +206,13 @@ begin
       emdc        => emdc,
       eint        => eint,
       erst        => erst,
+      -- AXI Chip2Chip physical link (FMC)
+      axi_c2c_selio_rx_data_in_0        => axi_c2c_selio_rx_data_in_0,
+      axi_c2c_selio_rx_diff_clk_in_p_0  => axi_c2c_selio_rx_diff_clk_in_p_0,
+      axi_c2c_selio_rx_diff_clk_in_n_0  => axi_c2c_selio_rx_diff_clk_in_n_0,
+      axi_c2c_selio_tx_data_out_0       => axi_c2c_selio_tx_data_out_0,
+      axi_c2c_selio_tx_diff_clk_out_p_0 => axi_c2c_selio_tx_diff_clk_out_p_0,
+      axi_c2c_selio_tx_diff_clk_out_n_0 => axi_c2c_selio_tx_diff_clk_out_n_0,
       --UART
       uart_rx      => dsurx,
       uart_tx      => dsutx,
